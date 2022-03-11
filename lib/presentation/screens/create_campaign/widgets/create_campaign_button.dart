a2f257df4df5ad1727a78acc580e22d2e4039969
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2022-03-11 16:26:50 +0700

    UI - Create Campaign 'Button Validation'

--- lib/presentation/screens/create_campaign/widgets/create_campaign_button.dart
+++ lib/presentation/screens/create_campaign/widgets/create_campaign_button.dart
@@ -12,51 +12,128 @@ class CreateCampaignButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CreateCampaignProgressCubit,
-        CreateCampaignProgressState>(builder: (context, state) {
-      if (state.index == 0) {
-        // Target Body
-        return CustomButtonLabel(
-          label: 'Next',
-          onTap: () => _setIndex(
-            index: 1,
-            context: context,
-          ),
+    return BlocBuilder<CreateCampaignDataCubit, CreateCampaignDataState>(
+      builder: (context, createCampaignDataState) {
+        return BlocBuilder<CreateCampaignProgressCubit,
+            CreateCampaignProgressState>(
+          builder: (context, createCampaignProgressState) {
+            if (createCampaignProgressState.index == 0) {
+              // Target Button
+              return builTargetButton(
+                context: context,
+                amount: createCampaignDataState.amount,
+                time: createCampaignDataState.time,
+              );
+            } else if (createCampaignProgressState.index == 1) {
+              // Title Button
+              return buildTitleButton(
+                title: createCampaignDataState.title,
+                image: createCampaignDataState.image,
+                context: context,
+              );
+            } else if (createCampaignProgressState.index == 2) {
+              // Description Button
+              return buildDescriptionButton(
+                description: createCampaignDataState.description,
+                context: context,
+              );
+            } else {
+              // Confirmation Button
+              return CustomButtonLabel(
+                label: 'Create Campaign',
+                onTap: () => _onCreateCampaign(
+                  context: context,
+                ),
+              );
+            }
+          },
         );
-      } else if (state.index == 1) {
-        // Title Body
-        return _buildDoubleButtons(
-          onFirstButton: () => _setIndex(
-            context: context,
-            index: 0,
-          ),
-          onSecondButton: () => _setIndex(
-            context: context,
-            index: 2,
-          ),
-        );
-      } else if (state.index == 2) {
-        // Description Body
-        return _buildDoubleButtons(
-          onFirstButton: () => _setIndex(
-            context: context,
-            index: 1,
-          ),
-          onSecondButton: () => _setIndex(
-            context: context,
-            index: 3,
-          ),
-        );
-      } else {
-        // Confirmation Body
-        return CustomButtonLabel(
-          label: 'Create Campaign',
-          onTap: () => _onCreateCampaign(
-            context: context,
-          ),
-        );
-      }
-    });
+      },
+    );
+  }
+
+  Widget builTargetButton({
+    required int? time,
+    required double? amount,
+    required BuildContext context,
+  }) {
+    if (time != null && amount != null) {
+      return CustomButtonLabel(
+        label: 'Next',
+        onTap: () => _setIndex(
+          index: 1,
+          context: context,
+        ),
+      );
+    } else {
+      return CustomButtonLabel(
+        label: 'Next',
+        backgroundColor: UniversalColor.gray4,
+        borderColor: UniversalColor.gray4,
+        onTap: () {
+          // TODO => Handle alert target data null
+        },
+      );
+    }
+  }
+
+  Widget buildTitleButton({
+    required String? title,
+    required String? image,
+    required BuildContext context,
+  }) {
+    if (title != null && image != null) {
+      return _buildDoubleButtons(
+        onFirstButton: () => _setIndex(
+          context: context,
+          index: 0,
+        ),
+        onSecondButton: () => _setIndex(
+          context: context,
+          index: 2,
+        ),
+      );
+    } else {
+      return _buildDoubleButtons(
+        onFirstButton: () => _setIndex(
+          context: context,
+          index: 0,
+        ),
+        onSecondButton: () {
+          // TODO => Handle alert title data null
+        },
+        isDisableNextButon: true,
+      );
+    }
+  }
+
+  Widget buildDescriptionButton({
+    required String? description,
+    required BuildContext context,
+  }) {
+    if (description != null) {
+      return _buildDoubleButtons(
+        onFirstButton: () => _setIndex(
+          context: context,
+          index: 1,
+        ),
+        onSecondButton: () => _setIndex(
+          context: context,
+          index: 3,
+        ),
+      );
+    } else {
+      return _buildDoubleButtons(
+        onFirstButton: () => _setIndex(
+          context: context,
+          index: 1,
+        ),
+        onSecondButton: () {
+          // TODO => Handle alert description data null,
+        },
+        isDisableNextButon: true,
+      );
+    }
   }
 
   void _setIndex({
@@ -68,17 +145,20 @@ class CreateCampaignButton extends StatelessWidget {
 
   void _onCreateCampaign({
     required BuildContext context,
-  }) {}
+  }) {
+    // TODO => connect Create campaign to server
+  }
 
   Widget _buildDoubleButtons({
     required Function() onFirstButton,
     required Function() onSecondButton,
+    bool isDisableNextButon = false,
   }) {
     return Row(
       children: [
         Flexible(
           child: CustomButtonLabel(
-            label: 'Cancel',
+            label: 'Back',
             backgroundColor: UniversalColor.gray4,
             borderColor: UniversalColor.gray4,
             onTap: onFirstButton,
@@ -90,6 +170,12 @@ class CreateCampaignButton extends StatelessWidget {
         Flexible(
           child: CustomButtonLabel(
             label: 'Next',
+            backgroundColor: (isDisableNextButon)
+                ? UniversalColor.gray4
+                : UniversalColor.green4,
+            borderColor: (isDisableNextButon)
+                ? UniversalColor.gray4
+                : UniversalColor.green4,
             onTap: onSecondButton,
           ),
         ),
