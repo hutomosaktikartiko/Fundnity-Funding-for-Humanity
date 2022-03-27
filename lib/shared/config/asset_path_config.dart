3e99c1bcf74228336b27406e0617549428a46e01
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2022-03-27 17:16:49 +0700

    Add asset_path_config

---
+++ lib/shared/config/asset_path_config.dart
@@ -0,0 +1,16 @@
+class AssetPathConfig {
+  AssetPathConfig._();
+  
+  static const String contractCampaignPath = 'assets/Campaign.json';
+  static const String contractCrowdfundingPath = 'assets/Crowdfunding.json';
+  static const String connectionErrorPath = 'assets/images/connection_error.png';
+  static const String emptyPath = 'assets/images/empty.svg';
+  static const String notificationEmptyPath = 'assets/images/notification_empty.png';
+  static const String logoPath = 'assets/images/logo.png';
+  static const String placeholderPath = 'assets/images/placeholder.png';
+
+  // onboarding
+  static const String onBoardingFirstPath = 'assets/images/onboarding/first.png';
+  static const String onBoardingSecondPath = 'assets/images/onboarding/second.png';
+  static const String onBoardingThirdPath = 'assets/images/onboarding/third.png';
+}
