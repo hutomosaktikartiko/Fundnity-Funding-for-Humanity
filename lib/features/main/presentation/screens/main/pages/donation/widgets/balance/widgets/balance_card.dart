1959deb3a767d688edfc255549d9f7e47e1ef925
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2022-04-04 14:03:28 +0700

    My Balance in Ether and UI - add balance

--- lib/features/main/presentation/screens/main/pages/donation/widgets/balance/widgets/balance_card.dart
+++ lib/features/main/presentation/screens/main/pages/donation/widgets/balance/widgets/balance_card.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ndialog/ndialog.dart';
 import 'package:web3dart/web3dart.dart';
 
+import '../../../../../../../../../../core/utils/screen_navigator.dart';
 import '../../../../../../../../../../shared/config/custom_color.dart';
 import '../../../../../../../../../../shared/config/custom_text_style.dart';
+import '../../../../../../../../../../shared/extension/big_int_parsing.dart';
 import '../../../../../../../../../../shared/extension/string_parsing.dart';
+import '../../../../../../../../../../shared/widgets/button/custom_button_label.dart';
 import '../../../../../../../../../../shared/widgets/custom_box_shadow.dart';
+import '../../../../../../../../../../shared/widgets/custom_dialog.dart';
 import '../../../../../../../../../../shared/widgets/disable_layer.dart';
 import '../../../../../../../../../../shared/widgets/widget_sized.dart';
 
@@ -47,27 +53,51 @@ class _BalanceCardState extends State<BalanceCard> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "${widget.address ?? ''}".walletAddressSplit(),
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 13,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      "${widget.amount?.getInEther ?? 0} ETH",
-                      style: TextStyle(
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${widget.address ?? ''}".walletAddressSplit(),
+                        style: TextStyle(
                           color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
+                          fontSize: 13,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          // TODO: Fix 'ETH' text
+                          Expanded(
+                            child: Text(
+                              "${widget.amount?.getInWei.etherInWeiToEther() ?? 0}",
+                              overflow: TextOverflow.clip,
+                              maxLines: 1,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            " ETH",
+                            overflow: TextOverflow.clip,
+                            maxLines: 1,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
                 ),
                 Material(
                   color: Colors.white,
@@ -75,7 +105,7 @@ class _BalanceCardState extends State<BalanceCard> {
                     borderRadius: BorderRadius.circular(8),
                   ),
                   child: InkWell(
-                    onTap: () => _openAddBalanceBottomSheet(context),
+                    onTap: () => _openAddBalanceDialog(context),
                     borderRadius: BorderRadius.circular(8),
                     child: Padding(
                       padding: const EdgeInsets.symmetric(
@@ -107,7 +137,100 @@ class _BalanceCardState extends State<BalanceCard> {
     );
   }
 
-  void _openAddBalanceBottomSheet(BuildContext context) {
-    // TODO => Open Add Balance BottomSheet
+  void _openAddBalanceDialog(BuildContext context) {
+    NAlertDialog(
+      dialogStyle: DialogStyle(
+        contentPadding: EdgeInsets.all(24),
+        borderRadius: BorderRadius.circular(6),
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            "How to get rinkeby balance?",
+            style: CustomTextStyle.gray2TextStyle.copyWith(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            "copy your address and go to rinkeby faucet like https://faucets.chain.link/rinkeby to get your rinkeby balance",
+            textAlign: TextAlign.center,
+            style: CustomTextStyle.gray2TextStyle.copyWith(
+              fontSize: 13,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 8,
+                    vertical: 12,
+                  ),
+                  decoration: BoxDecoration(
+                    color: UniversalColor.gray6,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text(
+                    widget.address.toString(),
+                    overflow: TextOverflow.ellipsis,
+                    style: CustomTextStyle.gray2TextStyle.copyWith(
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              GestureDetector(
+                onTap: () => _copyAddress(context),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 8,
+                    vertical: 9,
+                  ),
+                  decoration: BoxDecoration(
+                    color: UniversalColor.gray6,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Icon(
+                    Icons.copy,
+                    size: 20,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          CustomButtonLabel(
+            label: "Copy",
+            fontSize: 12,
+            borderRadius: 6,
+            backgroundColor: UniversalColor.green4,
+            onTap: () => _copyAddress(context),
+          ),
+        ],
+      ),
+    ).show(context);
+  }
+
+  void _copyAddress(BuildContext context) {
+    Clipboard.setData(ClipboardData(text: widget.address?.toString()));
+    CustomDialog.showToast(
+      context: context,
+      message: "Address copied to clipboard",
+      backgroundColor: UniversalColor.green4,
+    );
+    ScreenNavigator.closeScreen(context);
   }
 }
--- lib/shared/extension/big_int_parsing.dart
+++ lib/shared/extension/big_int_parsing.dart
@@ -11,4 +11,9 @@ extension BigIntParsing on BigInt? {
     if (target == null) return 0.0;
     return this!.toDouble() / target.toDouble() * 100;
   }
+
+  double etherInWeiToEther() {
+    if (this == null) return 0.0;
+    return this!.toInt() / 1000000000000000000;
+  }
 }
