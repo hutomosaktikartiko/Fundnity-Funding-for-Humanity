6da0d313437956aad13f592b1114588e15fbd80f
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2022-04-17 16:13:38 +0800

    Update contract

--- lib/shared/config/contract_config.dart
+++ lib/shared/config/contract_config.dart
@@ -1,6 +1,7 @@
 class ContractConfig {
   static const String crowdfunding = "0xf7A3B293D4f5047A22e0f7E57F6c12f4512abF53";
-  static const String crowdfundingInfura = "0x1Ded08231B84174985a821b419ad143cA8C7D0a9";
+  static const String crowdfundingInfura = "0x07851a4e3eF6cDA70E612D6392CdFf3B4017BD7C";
+  // static const String crowdfundingInfura = "0x1Ded08231B84174985a821b419ad143cA8C7D0a9";
   // static const String crowdfundingInfura = "0x691FB8938695be41F94B47220d1090199E3FA531";
   static const String crowdfundingInfuraLast = "0xAd9811754735aDcF5E4FD9c2A4F872e1840F8E8A";
   static const String cloudStorage = "0x4EE9aA39F40526F342725E77A6FBb9188319D4e9";
