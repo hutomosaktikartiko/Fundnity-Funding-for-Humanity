6675ecf9a38a0bcf07ef4ede18a03631edfc57ba
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2022-05-05 18:33:04 +0800

    Show all main campaigns

--- lib/features/main/presentation/screens/main/pages/donation/widgets/main_campaign/states/loaded.dart
+++ lib/features/main/presentation/screens/main/pages/donation/widgets/main_campaign/states/loaded.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../../../../../../core/utils/screen_navigator.dart';
 import '../../../../../../../../../../shared/config/custom_text_style.dart';
 import '../../../../../../../../../../shared/config/size_config.dart';
 import '../../../../../../../../../../shared/widgets/campaign_card/horizontal_campaign_card.dart';
 import '../../../../../../../../../../shared/widgets/widget_with_default_horizontal_padding.dart';
 import '../../../../../../../../data/models/campaign_model.dart';
+import '../../../../../../all_campaigns/all_campaigns_screen.dart';
 import '../../view_all_widget.dart';
 
 class Loaded extends StatelessWidget {
@@ -31,7 +33,9 @@ class Loaded extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              ViewAllWidget(onTap: _openAllMainCampaignScreen),
+              ViewAllWidget(
+                onTap: () => _openAllMainCampaignScreen(context),
+              ),
             ],
           ),
         ),
@@ -39,31 +43,35 @@ class Loaded extends StatelessWidget {
           scrollDirection: Axis.horizontal,
           padding: EdgeInsets.symmetric(vertical: 10),
           child: Row(
-            children: ((campaigns.length > 5) ? campaigns.sublist(0, 5) : campaigns)
-                .asMap()
-                .map(
-                  (key, campaign) => MapEntry(
-                    key,
-                    Padding(
-                      padding: EdgeInsets.only(
-                        left: (key == 0) ? SizeConfig.defaultMargin : 15,
-                        right: (key == campaigns.length - 1 || key == 4) ? SizeConfig.defaultMargin : 0,
+            children:
+                ((campaigns.length > 5) ? campaigns.sublist(0, 5) : campaigns)
+                    .asMap()
+                    .map(
+                      (key, campaign) => MapEntry(
+                        key,
+                        Padding(
+                          padding: EdgeInsets.only(
+                            left: (key == 0) ? SizeConfig.defaultMargin : 15,
+                            right: (key == campaigns.length - 1 || key == 4)
+                                ? SizeConfig.defaultMargin
+                                : 0,
+                          ),
+                          child: HorizontalCampaignCard(
+                            campaign: campaign,
+                          ),
+                        ),
                       ),
-                      child: HorizontalCampaignCard(
-                        campaign: campaign,
-                      ),
-                    ),
-                  ),
-                )
-                .values
-                .toList(),
+                    )
+                    .values
+                    .toList(),
           ),
         ),
       ],
     );
   }
 
-  void _openAllMainCampaignScreen() {
-    // TODO => Navigator to Main Donation Screen
+  void _openAllMainCampaignScreen(BuildContext context) {
+    ScreenNavigator.startScreen(context,
+        AllCampaignsScreen(campaigns: campaigns, title: "Need to Help First"));
   }
 }
