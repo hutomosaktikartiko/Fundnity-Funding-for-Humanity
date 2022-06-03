0e4e41c2b04e400059e19d40bea4fb6e658ae79d
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2022-06-04 00:17:23 +0800

    First Release 🔥

--- android/app/src/main/kotlin/com/hutomosaktikartiko/fundnity/MainActivity.kt
+++ android/app/src/main/kotlin/com/hutomosaktikartiko/fundnity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.hutomosaktikartiko.crowdfunding
+package com.hutomosaktikartiko.fundnity
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
